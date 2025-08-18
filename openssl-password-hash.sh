bb7fc4a045be7d5f51c1e97887f1f4e7c409b357
David Vogel <david@davidvogel.xyz>
2025-08-17 18:50:58 -0600

    UPDATE: simplified the script, in order to make it public

--- openssl-password-hash.sh
+++ openssl-password-hash.sh
@@ -2,14 +2,12 @@
 
 ask_static_salt() {
     read -r -p "Please provide a static salt. " static_salt
-
-    echo "$static_salt"
 }
 
-gen_pass_words() {
-    static_salt=$(ask_static_salt)
+main() {
+    ask_static_salt
 
     openssl passwd -salt $static_salt -6
 }
 
-gen_pass_words
+main
